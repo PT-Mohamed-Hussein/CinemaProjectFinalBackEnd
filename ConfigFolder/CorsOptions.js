0acd4a214a65399f3730f423a50d8ccdabe176b0
Diesel <pt.mohamed.ahmed@gmail.com>
2023-03-30 02:34:25 +0200

    add new allowed origin

--- ConfigFolder/CorsOptions.js
+++ ConfigFolder/CorsOptions.js
@@ -2,10 +2,10 @@ const allowedOrigins = require('./AllowedOrigins')
 
 const corsOptions = {
     origin: (origin, callback) => {
-        console.log(origin)
         if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
             callback(null, true)
         } else {
+            console.log(origin)
             callback(new Error('Not Allowed With Cors'))
         }
     },
