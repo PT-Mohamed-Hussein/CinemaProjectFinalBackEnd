c0d4221fc29700e80f54adfbe0a640e9c306008c
Diesel <pt.mohamed.ahmed@gmail.com>
2023-03-30 14:33:59 +0200

    cookie issue

--- middleware/credentials.js
+++ middleware/credentials.js
@@ -1,10 +1,7 @@
 const allowedOrigins = require('../ConfigFolder/AllowedOrigins')
 
 const credentials = (req, res, next) => {
-    const origin = req.headers.origin
-    if (allowedOrigins.includes(origin) || !origin) {
-        res.header('Access-Control-Allow-Credentials', true)
-    }
+    res.header('Access-Control-Allow-Credentials', true)
     next()
 }
 
