ed6a98e4376de12f50e78d5aa4e1b8318aa4ffbb
Diesel <pt.mohamed.ahmed@gmail.com>
2023-03-30 03:04:58 +0200

    add new allowed origin

--- ConfigFolder/AllowedOrigins.js
+++ ConfigFolder/AllowedOrigins.js
@@ -1,5 +1,5 @@
 const allowedOrigins = [
-    'https://serene-moxie-450fe7.netlify.app/',
+    'https://serene-moxie-450fe7.netlify.app',
     'https://www.yoursite.com',
     'http://127.0.0.1:3000',
     'http://localhost:3000'
